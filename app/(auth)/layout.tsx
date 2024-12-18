8841d0d820f040750bee428954fc0062e7f95408
Wesdell <wesdell17@gmail.com>
2024-12-18 16:56:17 -0500

    Markup auth and root layout and add clerkprovider component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css";
 
 export const metadata = {
     title: "Threads",
-    description: ""
+    description: "Meta Threads Application"
 };
+
+const inter = Inter({subsets: ["latin"]});
 
 export default function AuthLayout({children}: {children: React.ReactNode}) {
     return(
-        // <ClerkProvider></ClerkProvider>
-        <>
-        <div></div>
-        </>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
