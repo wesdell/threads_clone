321a67bf9f364b96a14acb6586e333573399c0d4
wesdell <wesdell17@gmail.com>
2024-12-18 12:13:11 -0500

    Prepare auth routes structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export const metadata = {
+    title: "Threads",
+    description: ""
+};
+
+export default function AuthLayout({children}: {children: React.ReactNode}) {
+    return(
+        // <ClerkProvider></ClerkProvider>
+        <>
+        <div></div>
+        </>
+    );
+}
