dee46f0deb6a2d9fcecbddd8e801c3cba80fde12
Wesdell <wesdell17@gmail.com>
2025-01-16 20:53:12 -0500

    Markup create thread page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { PostThread } from "@/components/forms/PostThread";
+
+export default async function CreateThread() {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+      <PostThread userId={userInfo._id} />
+    </>
+  );
+}
