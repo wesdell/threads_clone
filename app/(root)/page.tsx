d39729db3ceb44e8715bd92bb1b4dca47a401f32
Wesdell <wesdell17@gmail.com>
2025-01-17 20:27:53 -0500

    Initialize variable to fetch all threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { fetchThreads } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+  const threads = await fetchThreads(1, 30);
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
