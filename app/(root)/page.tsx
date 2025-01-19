f08ed2a1fee9ee0de5c34639ff364d08e492ab7a
Wesdell <wesdell17@gmail.com>
2025-01-19 17:01:01 -0500

    Markup home page and fecthing user threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,35 @@
+import { currentUser } from "@clerk/nextjs";
 import { fetchThreads } from "@/lib/actions/thread.actions";
+import { ThreadCard } from "@/components/cards";
 
 export default async function Home() {
-  const threads = await fetchThreads(1, 30);
+  const { threads } = await fetchThreads(1, 30);
+  const user = await currentUser();
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread.id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
