9204afad12edb96d40313b835a10f0510350b0e7
Wesdell <wesdell17@gmail.com>
2025-01-21 20:39:56 -0500

    Change route depending on the actual route a user is in

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -3,12 +3,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 import { sidebarLinks } from "@/constants";
 
 export const LeftSideBar = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -17,6 +18,10 @@ export const LeftSideBar = () => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === "/profile") {
+            link.route = `${link.route}/${userId}`;
+          }
           return (
             <Link
               href={link.route}
