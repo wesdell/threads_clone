58aa0f90b4c66032c807c6fb3a4381f2eaaef91e
Wesdell <wesdell17@gmail.com>
2025-01-22 17:14:20 -0500

    Markup new Profile page

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,47 @@
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { UserCard } from "@/components/cards";
+
+export default async function Search() {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="mb-10 head-text">Search</h1>
+      <div className="mb-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((user) => (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                username={user.username}
+                imgUrl={user.image}
+                userType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+}
