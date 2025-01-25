e362485c610318640ad51888cf34ec296bfc9edf
Wesdell <wesdell17@gmail.com>
2025-01-24 19:50:41 -0500

    Fix auth pages style layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,20 +4,26 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "../globals.css";
 
 export const metadata = {
-    title: "Threads",
-    description: "Meta Threads Application"
+  title: "Threads",
+  description: "Meta Threads Application",
 };
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
-export default function AuthLayout({children}: {children: React.ReactNode}) {
-    return(
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
-    );
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
